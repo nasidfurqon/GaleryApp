8b5d59208cc51b3eeecdc1c9a7f83edf0a2f9be3
Khoirul Nasid Furqon <nasidfurqon@gmail.com>
2025-01-09 23:53:30 +0700

    Content: Add a Surface Above the Image

--- app/src/main/java/com/example/galeryapp/MainActivity.kt
+++ app/src/main/java/com/example/galeryapp/MainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -56,14 +61,20 @@ fun ImageText(ImageId: Int,
 
     Column(modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally){
-        Image(
-            painter = painterResource(ImageId),
-            contentDescription = stringResource(ImageDesc),
-            modifier = Modifier
-                .width(220.dp)
-        )
+        Surface(modifier = Modifier
+
+            .shadow(elevation = 5.dp),
+            border = BorderStroke(1.dp, color = Color.Gray)){
+            Image(
+                painter = painterResource(ImageId),
+                contentDescription = stringResource(ImageDesc),
+                modifier = Modifier
+                    .width(270.dp)
+                    .padding(30.dp)
+            )
+        }
         Spacer(
-            Modifier.height(60.dp)
+            Modifier.height(40.dp)
         )
         Text(
             text = stringResource(PlaceId),
