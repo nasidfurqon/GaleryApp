b529a5ebe180ad2e1c0ae2c6188616bd4a94829b
Khoirul Nasid Furqon <nasidfurqon@gmail.com>
2025-01-14 14:49:25 +0700

    Content: Add Shape for Content Image and Card

--- app/src/main/java/com/example/galeryapp/MainActivity.kt
+++ app/src/main/java/com/example/galeryapp/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -153,15 +154,16 @@ fun ImageText(ImageId: Int,
     Column(modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally){
         Surface(modifier = Modifier
-
-            .shadow(elevation = 10.dp),
-            border = BorderStroke(1.dp, color = Color.Gray)){
+            .shadow(elevation = 4.dp, shape = RoundedCornerShape(16.dp)),
+            border = BorderStroke(1.dp, color = MaterialTheme.colorScheme.secondary),
+            shape = RoundedCornerShape(16.dp)){
             Image(
                 painter = painterResource(ImageId),
                 contentDescription = stringResource(ImageDesc),
                 modifier = Modifier
                     .width(300.dp)
                     .padding(30.dp)
+                    .clip(shape = RoundedCornerShape(8.dp)),
             )
         }
         Spacer(
